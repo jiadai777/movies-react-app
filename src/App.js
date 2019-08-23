5ab8dbdbfc512eb1d062d9dc98fb11d4f016ffc9
Jiadai Liu <jliuapp@gmail.com>
2019-08-22 17:49:24 -0700

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+import MoviesList from './components/MoviesList';
+
+function App() {
+  return (
+    <React.Fragment>
+      <MoviesList />
+    </React.Fragment>
+  );
+}
+
+export default App;
